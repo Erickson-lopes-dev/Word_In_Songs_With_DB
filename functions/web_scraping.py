9fc1cd5c048f35c0f4d9df1f5450ab9b943f5c73
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-20 16:49:54 -0300

    add: coletando urls das letras, exibindo se nenhum item foi encontrado.

--- functions/web_scraping.py
+++ functions/web_scraping.py
@@ -7,19 +7,31 @@ def main_artist_page(artist):
     req_get = requests.get(f'https://www.vagalume.com.br/{artist.replace(" ", "-").lower()}/')
 
     if req_get.status_code == 200:
-        # Cria o obj python do corpo html recebido com o beautifulsoup
-        obj_bs4 = BeautifulSoup(req_get.content, 'html.parser')
+        try:
+            # Cria o obj python do corpo html recebido com o beautifulsoup
+            obj_bs4 = BeautifulSoup(req_get.content, 'html.parser')
 
-        # busca o cunteudo dentro da clase 'nameMusic'
-        list_alfabet = obj_bs4.find(id='alfabetMusicList').find_all(class_='nameMusic')
-
-        return list_alfabet
+            # busca o cunteudo dentro da clase 'nameMusic' que contem o link para cada musica
+            list_alfabet = obj_bs4.find(id='alfabetMusicList').find_all(class_='nameMusic')
+        except:
+            pass
+        else:
+            # retorna o obj
+            return list_alfabet
 
     # Se caso o artista não for encontrado retorna uma lsita vazia
+    print(f'Nenhum item encontrado ! {artist}')
     return []
 
 
+def searching_lyrics(artist):
+    data_page_artist = main_artist_page(artist)
+
+    if data_page_artist:
+        for item in data_page_artist:
+            print(item.text, 'https://www.vagalume.com.br' + item.attrs['href'])
+
+
 if __name__ == '__main__':
-    for item in main_artist_page('raul'):
-        print(item.attrs['href'])
-        print(item.text)
+
+    searching_lyrics('')
